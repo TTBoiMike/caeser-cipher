573d6ce3c8a38af652b8aa942ade2f93157e6e5b
TTBoiMike <michaelthorpe747@hotmail.com>
2020-11-19 16:43:29 +0000

    improves encoding for later letters in the alphabet

--- caesercipher.js
+++ caesercipher.js
@@ -1,5 +1,5 @@
 // global variables
-let alphabet = ["a", "b", "c", "d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x", "y","z"];
+let alphabet = ["a", "b", "c", "d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x", "y","z", "a", "b", "c", "d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x", "y","z"];
 let letterRegEx = /[^a-z]/;
 
 // create function that takes the input message and returns a ciphered message
